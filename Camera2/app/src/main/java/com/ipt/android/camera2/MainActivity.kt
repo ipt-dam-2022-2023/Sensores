51e0a96ca0a49d7094981cf579e7e71ba7232c7b
pauloagsantos <pauloagsantos@gmail.com>
2022-10-28 12:55:06 +0100

    limpeza de codigo

--- Camera2/app/src/main/java/com/ipt/android/camera2/MainActivity.kt
+++ Camera2/app/src/main/java/com/ipt/android/camera2/MainActivity.kt
@@ -8,12 +8,8 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
-import android.view.View
-import android.view.View.OnLongClickListener
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import java.io.FileDescriptor
@@ -21,78 +17,59 @@ import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
-    var frame: ImageView? = null
+    private var frame: ImageView? = null
+    private var imageUri: Uri? = null
+    private val RESULT_LOAD_IMAGE = 123
+    private val IMAGE_CAPTURE_CODE = 654
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         frame = findViewById(R.id.imageView)
-
-
-        //TODO ask for permission of camera upon first launch of application
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                == PackageManager.PERMISSION_DENIED) {
-                val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                requestPermissions(permission, 112)
-            }
+        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
+            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            requestPermissions(permission, 112)
+        }
+        frame?.setOnLongClickListener {
+            openCamera()
+            true
         }
 
-
-        //TODO captue image using camera
-        frame?.setOnLongClickListener(OnLongClickListener {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    == PackageManager.PERMISSION_DENIED) {
-                    val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    requestPermissions(permission, 121)
-                } else {
-                    openCamera()
-                }
-            } else {
-                openCamera()
-            }
-            true
-        })
-
-
-        //TODO chose image from gallery
-        frame?.setOnClickListener(View.OnClickListener {
-            val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE)
-        })
+        frame?.setOnClickListener {
+            openGallery()
+        }
     }
 
-    var image_uri: Uri? = null
-    private val RESULT_LOAD_IMAGE = 123
-    val IMAGE_CAPTURE_CODE = 654
+    private fun openGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE)
+    }
 
-    //TODO opens camera so that user can capture image
+
     private fun openCamera() {
         val values = ContentValues()
         values.put(MediaStore.Images.Media.TITLE, "New Picture")
         values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
-        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
         val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
         startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == IMAGE_CAPTURE_CODE && resultCode == Activity.RESULT_OK) {
-            //imageView.setImageURI(image_uri);
-            val bitmap = uriToBitmap(image_uri!!)
+            val bitmap = uriToBitmap(imageUri!!)
             frame?.setImageBitmap(bitmap)
         }
         if (requestCode == RESULT_LOAD_IMAGE && resultCode == Activity.RESULT_OK && data != null) {
-            image_uri = data.data
-            //imageView.setImageURI(image_uri);
-            val bitmap = uriToBitmap(image_uri!!)
+            imageUri = data.data
+            val bitmap = uriToBitmap(imageUri!!)
             frame?.setImageBitmap(bitmap)
         }
     }
 
-    //TODO takes URI of the image and returns bitmap
+
     private fun uriToBitmap(selectedFileUri: Uri): Bitmap? {
         try {
             val parcelFileDescriptor = contentResolver.openFileDescriptor(selectedFileUri, "r")
