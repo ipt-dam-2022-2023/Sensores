5c0f699c073514531dadd9336004bbb9bd399fb9
pauloagsantos <pauloagsantos@gmail.com>
2022-10-28 10:44:50 +0100

    add camara do fragment 2

--- ExemploFragmentos/app/src/main/java/com/example/android/exemplofragmentos/fragmentos/Fragment2.kt
+++ ExemploFragmentos/app/src/main/java/com/example/android/exemplofragmentos/fragmentos/Fragment2.kt
@@ -1,14 +1,25 @@
 package com.example.android.exemplofragmentos.fragmentos
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.android.exemplofragmentos.R
 
 
 class Fragment2 : Fragment() {
+
+    lateinit var button: Button
+    lateinit var imageView: ImageView
+
 
 
 
@@ -16,8 +27,25 @@ class Fragment2 : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_2, container, false)
+        var view:View = inflater.inflate(R.layout.fragment_2, container, false)
+        button = view.findViewById(R.id.button)
+        imageView = view.findViewById(R.id.imageView)
+
+        button.setOnClickListener {
+            capturePhoto()
+        }
+        return view
+    }
+
+    fun capturePhoto() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        resultLauncher.launch(cameraIntent)
+    }
+    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            imageView.setImageBitmap(data?.extras?.get("data") as Bitmap)
+        }
     }
 
 
